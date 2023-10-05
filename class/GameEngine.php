8e4b917d2f910df6cd76f0e547c754546b218297
Aleksandr Harisov <bwb-a.harisov@fondespierre.com>
2023-10-05 17:15:21 +0200

    finished battle

--- class/GameEngine.php
+++ class/GameEngine.php
@@ -39,21 +39,31 @@ class GameEngine {
         return $this->combattants[$this->getId()];
     }
 
+    // method for getting all players
+    public function getToutesJoueurs() {
+        // get random player and return this object
+        return $this->combattants;
+    }
+
     // method for every game tour
     public function tourDeJeu() {
         
         // every player attacks during one tour
         foreach($this->combattants as $combattant) {
 
+            sleep(1);
             // only alive player can attack
             if ($combattant->__get("pv") > 0) {
                 // get random victim
                 $cible = $this->getJoueur();
 
                 // the victim should not be the same player
-                if ($cible != $combattant) {
+                if ($cible != $combattant && $cible->pv > 0) {
                     echo $combattant->attaquer($cible, false);
                 }
+                ob_flush();
+                flush();
+
             } else {
                 // clean the battle field
                 echo $this->nettoyerMort();
@@ -78,6 +88,18 @@ class GameEngine {
                 return '<h3 style="color: red;">' . $combattant->race . ' ' . $combattant->nom . ' quitte le champ de bataille</h3>';
             }
         }
+    }
+
+    public static function returnRefreshButtons($url) {
+        echo '<a href="'. $url .'" class="refresh-btn border-2 rounded-lg absolute p-5 border-purple-600 bg-purple-600 hover:bg-purple-800 text-lg uppercase text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+        </svg></a>';
+
+        echo '<a href="http://localhost/poo_thegame/" class="return-home border-2 rounded-lg p-5 border-rose-600 hover:bg-rose-800 bg-rose-600 text-lg"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+        <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>
+        </svg></a>';
     }
 
     // method for finishing the game
--- index.php
+++ index.php
@@ -20,29 +20,10 @@
         require("class/Elfe.php");
         require("class/GameEngine.php");
         require("class/Duel.php");
-
-        //------------- game engine start ---------------------------
-        /*
-        echo "<h2>La bataille commence</h2>";
-
-        $game = new GameEngine();
-        $game->addCombattant(new Humain("Loic")); 
-        $game->addCombattant(new Orc("Yannick"));
-        $game->addCombattant(new Elfe("Jonathan"));
-        $game->addCombattant(new Humain("Test1"));
-        $game->addCombattant(new Elfe("Test2"));
-        $game->addCombattant(new Orc("Test3"));
-        $game->addCombattant(new Orc("Test4"));
-        $game->addCombattant(new Elfe("Test5"));
-
-        $game->start();
-        */
-        //var_dump($game->getJoueur());
-        //------------- game engine finish --------------------------
     ?>
 
     <main class="flex justify-center items-center ">
-        <div class="game-container <?php if (!$_GET) echo 'justify-center';?> border-double border-4 rounded-lg border-indigo-600">
+        <div class="game-container <?php if ($_GET && $_GET["game"] == "battle") echo 'game-container-big';?> <?php if (!$_GET) echo 'justify-center';?> border-double border-4 rounded-lg border-indigo-600">
             
             <?php if (!$_GET) {?>
                 
@@ -57,31 +38,49 @@
              
                         if ($_GET && $_GET["game"] == "duel") {
                             
+                            // generate players for duel
                             $domeDuTonnere = [
                                 Personnage::generatePersonnage(), 
                                 Personnage::generatePersonnage()
                             ];
 
-                            echo '<h1 class="text-5xl mb-10 ">Le duel commence !</h1>';
+                            echo "<h1 class='text-5xl mb-10'>Le duel commence !</h1>";
                             echo "<div class='battle-field'>";
+                            
+                            // show players on the filed
                             echo Personnage::showPersonnage($domeDuTonnere[0]);
                             echo Personnage::showPersonnage($domeDuTonnere[1]);
                             
                             echo "<div class='field-inner'>";
+                            // start duel
                             Duel::startDuel($domeDuTonnere);
-                            echo "</div>";
+                            echo "</div></div>";
 
-                            echo "</div>";
-
-                            echo '<a href="?game=duel" class="border-2 rounded-lg absolute p-5 border-purple-600 bg-purple-600 hover:bg-purple-800 text-lg uppercase text-white" style="bottom: 3em">commencer un nouvel duel</a>';
-                            echo '<a href="http://localhost/poo_thegame/" class="return-home border-2 rounded-lg p-5 border-rose-600 hover:bg-rose-800 bg-rose-600 text-lg"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
-                            <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>
-                          </svg></a>';
+                            GameEngine::returnRefreshButtons("?game=duel");
 
                         } else {
 
                             echo '<h1 class="text-5xl mb-10 ">La bataille commence !</h1>';
 
+                            $game = new GameEngine();
+
+                            // generate all players, min 3 and max 10
+                            for ($i = 1; $i <= rand(3, 10); $i++) {
+                                $game->addCombattant(Personnage::generatePersonnage()); 
+                            }
+
+                            echo "<div class='battle-field-big battle-count-".count($game->getToutesJoueurs())."'>";
+                            // show all players on the field
+                            foreach($game->getToutesJoueurs() as $jouyeur) {
+                                echo Personnage::showPersonnage($jouyeur);
+                            }
+                            
+                            echo "<div class='field-inner'>";
+                            $game->start();
+                            echo "</div>";
+
+                            GameEngine::returnRefreshButtons("?game=battle");
+                            echo "</div>";
                         }
                     }
             ?>
